60d75aa2e772513da4a04c8c1ad86024717cbdc4
neetu <neetuy1455@gmail.com>
2017-05-15 17:08:57 +0530

    change port in index

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ app.use(express.static('./client/dist/'));
 // tell the app to parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('port', (process.env.PORT || 5000));
 // routes
-const authRoutes = require('./server/routes/auth');
-app.use('/auth', authRoutes);
+// const authRoutes = require('./server/routes/auth');
+// app.use('/auth', authRoutes);
 
 // start the server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
+// });
 
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
